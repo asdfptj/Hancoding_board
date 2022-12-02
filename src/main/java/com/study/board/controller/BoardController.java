c547d1c7ac4ae9b2127b97d8724ff48e0e67327d
asdfptj <taejunb70@gmail.com>
2022-12-02 14:54:27 +0900

    file upload

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -8,7 +8,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+
 
 @Controller
 public class BoardController {
@@ -23,14 +25,14 @@ public class BoardController {
     }
 
     @PostMapping("/board/writepro")
-    public String boardWritePro(Board board, Model model) {
+    public String boardWritePro(Board board, Model model, MultipartFile file) throws Exception{
 
-        boardService.write(board);
+        boardService.write(board, file);
 
         model.addAttribute("message", "글 작성이 완료되었습니다.");
         model.addAttribute("searchUrl", "/board/list");
 
-    return "message";
+        return "message";
     }
 
     @GetMapping("/board/list")
@@ -62,18 +64,21 @@ public class BoardController {
                               Model model) {
 
         model.addAttribute("board", boardService.boardView(id));
-    return  "boardmodify";
+
+        return "boardmodify";
     }
 
     @PostMapping("/board/update/{id}")
-    public String boardUpdate(@PathVariable("id") Integer id,Board board, Model model) {
+    public String boardUpdate(@PathVariable("id") Integer id, Board board, MultipartFile file) throws Exception{
+
         Board boardTemp = boardService.boardView(id);
         boardTemp.setTitle(board.getTitle());
         boardTemp.setContent(board.getContent());
-        boardService.write(boardTemp);
-        model.addAttribute("message", "글 수정이 완료되었습니다.");
-        model.addAttribute("searchUrl", "/board/list");
-        return "message";
+
+        boardService.write(boardTemp, file);
+
+        return "redirect:/board/list";
+
     }
 
 }
